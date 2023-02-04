b0634b2348df6232d74374aca5b135fd6e78bdce
Bars0_o <maksterekhov1704@gmail.com>
2023-02-04 23:46:02 +0200

    scope

--- MarkovTest/Converters/PatternConverter.cs
+++ MarkovTest/Converters/PatternConverter.cs
@@ -2,78 +2,79 @@ using Newtonsoft.Json;
 using JsonException = Newtonsoft.Json.JsonException;
 using JsonSerializer = Newtonsoft.Json.JsonSerializer;
 
-namespace MarkovTest.Converters;
-
-public class PatternConverter : JsonConverter
+namespace MarkovTest.Converters
 {
-    public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+    public class PatternConverter : JsonConverter
     {
-        var obj = (Array)value;
-
-        writer.WriteStartArray();
-        for (var i = 0; i < obj.GetLength(0); i++)
+        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
         {
+            var obj = (Array)value;
+
             writer.WriteStartArray();
-            for (var j = 0; j < obj.GetLength(1); j++)
+            for (var i = 0; i < obj.GetLength(0); i++)
             {
-                writer.WriteValue(obj.GetValue(i, j));
+                writer.WriteStartArray();
+                for (var j = 0; j < obj.GetLength(1); j++)
+                {
+                    writer.WriteValue(obj.GetValue(i, j));
+                }
+
+                writer.WriteEndArray();
             }
 
             writer.WriteEndArray();
         }
 
-        writer.WriteEndArray();
-    }
-
-    public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
-    {
-        var matrix = new List<List<object>>();
-        matrix.Add(new List<object>());
-
-        if (reader.TokenType != JsonToken.StartArray)
-            throw new JsonException($"Invalid state - must be starting array , instead = {reader.TokenType}");
-        reader.Read();
-        if (reader.TokenType != JsonToken.StartArray)
-            throw new JsonException($"Invalid state - must be starting array , instead = {reader.TokenType}");
-
-        while (reader.Read())
+        public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
         {
-            if (reader.TokenType == JsonToken.EndArray)
+            var matrix = new List<List<object>>();
+            matrix.Add(new List<object>());
+
+            if (reader.TokenType != JsonToken.StartArray)
+                throw new JsonException($"Invalid state - must be starting array , instead = {reader.TokenType}");
+            reader.Read();
+            if (reader.TokenType != JsonToken.StartArray)
+                throw new JsonException($"Invalid state - must be starting array , instead = {reader.TokenType}");
+
+            while (reader.Read())
             {
-                reader.Read();
                 if (reader.TokenType == JsonToken.EndArray)
-                    break;
-                if (reader.TokenType == JsonToken.StartArray)
                 {
-                    matrix.Add(new List<object>());
                     reader.Read();
+                    if (reader.TokenType == JsonToken.EndArray)
+                        break;
+                    if (reader.TokenType == JsonToken.StartArray)
+                    {
+                        matrix.Add(new List<object>());
+                        reader.Read();
+                    }
                 }
+
+                matrix.Last().Add(reader.Value);
             }
 
-            matrix.Last().Add(reader.Value);
+            if (matrix.GroupBy(x => x.Count).Count() > 1)
+                throw new JsonException($"Invalid matrix form");
+
+            var mainType = objectType.MakeArrayType().GetGenericArguments()[0];
+
+            var xSize = matrix.Count;
+            var ySize = matrix.First().Count;
+            var result = new IEquatable<byte>[xSize, ySize];
+
+            for (var x = 0; x < xSize; x++)
+            for (var y = 0; y < ySize; y++)
+                result[x, y] = (byte)JsonConvert.DeserializeObject(matrix[x][y].ToString(), mainType); 
+
+            return result;
         }
 
-        if (matrix.GroupBy(x => x.Count).Count() > 1)
-            throw new JsonException($"Invalid matrix form");
-
-        var mainType = objectType.MakeArrayType().GetGenericArguments()[0];
-
-        var xSize = matrix.Count;
-        var ySize = matrix.First().Count;
-        var result = new IEquatable<byte>[xSize, ySize];
-
-        for (var x = 0; x < xSize; x++)
-        for (var y = 0; y < ySize; y++)
-            result[x, y] = (byte)JsonConvert.DeserializeObject(matrix[x][y].ToString(), mainType); 
-
-        return result;
-    }
-
-    public override bool CanConvert(Type objectType)
-    {
-        if (!objectType.IsArray) return false;
-        if (objectType.GetArrayRank() != 2) return false;
-        if (objectType.MakeArrayType().GetInterfaces().Contains(typeof(IEquatable<>))) return false;
-        return true;
+        public override bool CanConvert(Type objectType)
+        {
+            if (!objectType.IsArray) return false;
+            if (objectType.GetArrayRank() != 2) return false;
+            if (objectType.MakeArrayType().GetInterfaces().Contains(typeof(IEquatable<>))) return false;
+            return true;
+        }
     }
 }
